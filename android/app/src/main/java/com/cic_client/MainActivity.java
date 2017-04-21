5cdbc2381291beaf6b50c15c224fb054f77cb86e
Nealyang <nealyang231@gmail.com>
2017-04-21 13:30:39 +0800

    初始化项目

---
+++ android/app/src/main/java/com/cic_client/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cic_client;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "cic_client";
+    }
+}
