95bf0deb77aa127e237eb4d88b0db2483366baee
Nealyang <nealyang231@gmail.com>
2017-04-22 14:28:54 +0800

    新增功能

--- index.android.js
+++ index.android.js
@@ -18,6 +18,7 @@ export default class cic_client extends Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>
           首页
+          
         </Text>
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
