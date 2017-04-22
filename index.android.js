36092b7ecdbfb648f7655a7609647c873ae477bc
Nealyang <nealyang231@gmail.com>
2017-04-22 13:52:54 +0800

    首页制作

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class cic_client extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to Hello word!
+          首页
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
