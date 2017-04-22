7a68cf7d3602d0f9d9bf489d62bf9093f73f67c0
Nealyang <nealyang231@gmail.com>
2017-04-22 13:27:46 +0800

    学习git

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class cic_client extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Hello word!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
