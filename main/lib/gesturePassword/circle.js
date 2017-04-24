6579ea21c014e064e9dcaecc057cea166c1d89c5
张帆 <839826287@qq.com>
2017-04-24 17:25:43 +0800

    创建lib，放置修改源码的三方插件

---
+++ main/lib/gesturePassword/circle.js
@@ -0,0 +1,56 @@
+import React, { PropTypes, Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+
+export default class Circle extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let {color, fill, x, y, r, inner, outer} = this.props;
+
+        return (
+            <View style={[styles.outer,
+                        {left: x - r, top: y - r, width: 2 * r, height: 2 * r, borderRadius: r},
+                        fill && {borderColor: color},
+                        !outer && {borderWidth: 0}]}>
+
+            <View style={[
+                            !outer && styles.inner,
+                            {width: 2 * r / 3, height: 2 * r / 3, borderRadius: r / 3},
+                            fill ? {backgroundColor: color} : {backgroundColor:'#b2b2b2'}
+                        ]} />
+            </View>
+        )
+    }
+}
+
+Circle.propTypes = {
+    color: PropTypes.string,
+    fill: PropTypes.bool,
+    x: PropTypes.number,
+    y: PropTypes.number,
+    r: PropTypes.number,
+    inner: PropTypes.bool,
+    outer: PropTypes.bool
+}
+
+Circle.defaultProps = {
+    inner: true,
+    outer: true
+}
+
+const styles = StyleSheet.create({
+    outer: {
+        position: 'absolute',
+        borderColor: '#b2b2b2',
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inner: {
+        backgroundColor: '#8E91A8'
+    }
+})
+
+module.exports = Circle;    // for compatible with require only
