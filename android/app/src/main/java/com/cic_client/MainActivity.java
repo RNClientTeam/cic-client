f187ccb291b8ff8274d9d22b8e728cb932e086f2
张帆 <839826287@qq.com>
2017-04-25 16:28:15 +0800

    修复安卓和iOS启动白屏问题

--- android/app/src/main/java/com/cic_client/MainActivity.java
+++ android/app/src/main/java/com/cic_client/MainActivity.java
@@ -1,8 +1,17 @@
 package com.cic_client;
+import android.os.Bundle;
+import com.facebook.soloader.SoLoader;
 
+import com.cboy.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+        SoLoader.init(this, /* native exopackage */ false);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
