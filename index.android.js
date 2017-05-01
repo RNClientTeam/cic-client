71a4aa41fb0b1ba760fea41ff25e69ccc4d6a8dc
zhubin <foreverscmd@gmail.com>
2017-05-01 23:52:33 +0800

    小修改

--- index.android.js
+++ index.android.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import Entrance from './main/Entrance.js';
 
-export default class cic_client extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          首页
-          
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+class cic_client extends Component {
+    render() {
+        return (
+            <Entrance />
+        );
+    }
 }
 
 AppRegistry.registerComponent('cic_client', () => cic_client);
