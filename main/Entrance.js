0a96460b414d1f146f041239d541ad7cd0f42e4a
Nealyang <nealyang231@gmail.com>
2017-05-08 23:13:51 +0800

    storage

--- main/Entrance.js
+++ main/Entrance.js
@@ -4,23 +4,41 @@ import {
     View,
     Navigator,
     Text,
-    Image
+    Image,
+    AsyncStorage
 } from 'react-native';
 
 import Splash from 'react-native-splash-screen';
-
+import Storage from 'react-native-storage';
 import Login from './Login.js';
 
 export default class Entrance extends Component {
     componentDidMount() {
         Splash.hide();
+        let storage = new Storage({
+            // 最大容量，默认值1000条数据循环存储
+            size: 1000,
+
+            // 如果不指定则数据只会保存在内存中，重启后即丢失
+            storageBackend: AsyncStorage,
+
+            // 数据过期时间，默认一整天（1000 * 3600 * 24 毫秒），设为null则永不过期
+            defaultExpires: null,
+
+            // 读写时在内存中缓存数据。默认启用。
+            enableCache: true,
+
+        });
+
+        global.storage = storage;
     }
+
     render() {
-        return(
+        return (
             <Navigator
                 initialRoute={{
                     component: Login,
-                    name:'Login'
+                    name: 'Login'
                 }}
                 renderScene={(route, navigator) => {
                     return <route.component navigator={navigator}  {...route.params}/>;
@@ -30,7 +48,7 @@ export default class Entrance extends Component {
                         return Navigator.SceneConfigs.FadeAndroid;
                     }
                     return Navigator.SceneConfigs.PushFromRight;
-                }} />
+                }}/>
         );
     }
 }
--- main/Home/Home.js
+++ main/Home/Home.js
@@ -9,7 +9,7 @@ import {
     TouchableOpacity,
     ScrollView
 } from 'react-native';
-const  {width} = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 import StatusBar from '../Component/StatusBar'
 import MenuItems from './Component/MenuItems'
 import ManageState from './Component/ManageState'
@@ -21,42 +21,72 @@ import CameraPage from './Component/CameraPage';
 
 export default class Home extends Component {
     render() {
-        return(
-        <View>
-            <StatusBar notBack={true} navigator={this.props.navigator}>
-                <Image style={styles.logoStyle} source={require('../../resource/imgs/home/home_logo.png')}/>
-                <Text onPress={this.test.bind(this)} style={styles.logoText}>九州方圆</Text>
-                <View style={styles.operationViewStyle}>
-                    <TouchableOpacity onPress={this.onPressSweep.bind(this)}>
-                        <Image style={styles.sweepStyle}  source={require('../../resource/imgs/home/sweep.png')}/>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={this.onPresSigned.bind(this)}>
-                        <Image style={styles.signedStyle} source={require('../../resource/imgs/home/signed.png')}/>
-                    </TouchableOpacity>
-                </View>
-            </StatusBar>
-            <ScrollView>
-                <View style={styles.viewSty}>
-                    {/*菜单栏*/}
-                    <MenuItems navigator={this.props.navigator} />
-                    {/*公司经营状况*/}
-                    <ManageState/>
-                    {/*最新消息*/}
-                    <Notification/>
-                </View>
-            </ScrollView>
-        </View>
+        return (
+            <View>
+                <StatusBar notBack={true} navigator={this.props.navigator}>
+                    <Image style={styles.logoStyle} source={require('../../resource/imgs/home/home_logo.png')}/>
+                    <Text style={styles.logoText}>九州方圆</Text>
+                    <View style={styles.operationViewStyle}>
+                        <TouchableOpacity onPress={this.onPressSweep.bind(this)}>
+                            <Image style={styles.sweepStyle} source={require('../../resource/imgs/home/sweep.png')}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this.onPresSigned.bind(this)}>
+                            <Image style={styles.signedStyle} source={require('../../resource/imgs/home/signed.png')}/>
+                        </TouchableOpacity>
+                    </View>
+                </StatusBar>
+                <ScrollView>
+                    <View style={styles.viewSty}>
+                        {/*菜单栏*/}
+                        <MenuItems navigator={this.props.navigator}/>
+                        {/*公司经营状况*/}
+                        <ManageState/>
+                        {/*最新消息*/}
+                        <Notification/>
+                    </View>
+                </ScrollView>
+            </View>
         );
     }
-    test(){
-        let p = 'http://image.tianjimedia.com/imagelist/2009/190/caq4z56jadof.pdf';
-        let s = 'http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg';
-        DownLoadFc.DownLoadFc(p);
+
+    test() {
+//存数据
+        //  storage.save({
+        //      key:'neal',
+        //      data:{
+        //          name:'杨磊',
+        //          userId:'Neal',
+        //          token: 'some token'
+        //      }
+        //  });
+//删除数据
+        // storage.remove({
+        //     key: 'lastPage'
+        // });
+//取数据
+        // storage.load({
+        //     key:'neadl'
+        // }).then((res)=>{
+        //     alert(JSON.stringify(res))
+        // }).catch(err => {
+        //     console.warn(err.message);
+        //     switch (err.name) {
+//key没有找到值         case 'NotFoundError':
+        //             // TODO;
+        //             alert(1)
+        //             break;
+        //         case 'ExpiredError':
+        //             // TODO
+        //             alert(2)
+        //             break;
+        //     }
+        // })
     }
+
     /**
      * 扫一扫
      */
-    onPressSweep(){
+    onPressSweep() {
         const {navigator} = this.props;
         if (navigator) {
             navigator.push({
@@ -72,7 +102,7 @@ export default class Home extends Component {
     /**
      * 签到
      */
-    onPresSigned(){
+    onPresSigned() {
         this.props.navigator.push({
             component: Signed,
             name: 'Signed'
@@ -81,42 +111,42 @@ export default class Home extends Component {
 }
 
 const styles = StyleSheet.create({
-    navigationStyle:{
-        flexDirection:'row',
-        backgroundColor:'#216fd0',
-        width:width,
-        height:44,
-        alignItems:'center'
+    navigationStyle: {
+        flexDirection: 'row',
+        backgroundColor: '#216fd0',
+        width: width,
+        height: 44,
+        alignItems: 'center'
     },
-    logoStyle:{
-        width:width*0.1,
-        height:width*0.1,
-        marginLeft:5
+    logoStyle: {
+        width: width * 0.1,
+        height: width * 0.1,
+        marginLeft: 5
     },
-    logoText:{
-        color:'#fff',
-        fontWeight:"800",
-        marginLeft:6,
-        fontSize:15
+    logoText: {
+        color: '#fff',
+        fontWeight: "800",
+        marginLeft: 6,
+        fontSize: 15
     },
-    operationViewStyle:{
-        flexDirection:'row',
-        height:44,
-        position:'absolute',
-        right:10,
-        alignItems:'center'
+    operationViewStyle: {
+        flexDirection: 'row',
+        height: 44,
+        position: 'absolute',
+        right: 10,
+        alignItems: 'center'
     },
-    sweepStyle:{
-        width:width*0.05,
-        height:width*0.05
+    sweepStyle: {
+        width: width * 0.05,
+        height: width * 0.05
     },
-    signedStyle:{
-        width:width*0.05,
-        height:width*0.05,
-        marginLeft:width*0.07
+    signedStyle: {
+        width: width * 0.05,
+        height: width * 0.05,
+        marginLeft: width * 0.07
     },
     viewSty: {
-        flex:1,
-        backgroundColor:'#f2f2f2'
+        flex: 1,
+        backgroundColor: '#f2f2f2'
     }
 });
