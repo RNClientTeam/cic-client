15b9871d6d1ee2c9e7de092d98488eb6f27ae454
zhubin <foreverscmd@gmail.com>
2017-05-09 18:15:44 +0800

    计划编制-施工计划页面

---
+++ main/Home/Applications/ConstructionProgressPlan/Component/MyPlan.js
@@ -0,0 +1,123 @@
+/**
+ * Created by zhubin on 17/5/9.
+ */
+import React, {Component} from 'react'
+import {
+    View,
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity,
+    Text,
+    ScrollView,
+    Image,
+    ListView
+} from 'react-native'
+
+import {PullList} from 'react-native-pull';
+import LoadMore from "../../../../Component/LoadMore.js"
+import Reload from "../../../../Component/Reload.js"
+import MyPlanCell from "./MyPlanCell"
+
+const {width, height} = Dimensions.get('window');
+let dataArr = [
+    {
+        period: '准备设备',
+        state: '执行中',
+        principal: '杨磊',
+        time: '2017/11/11-2017/12/12'
+    },
+    {
+        period: '设备检测',
+        state: '执行中',
+        principal: '杨磊',
+        time: '2017/11/11-2017/12/12'
+    },
+    {
+        period: '开始施工',
+        state: '执行中',
+        principal: '杨磊',
+        time: '2017/11/11-2017/12/12'
+    },
+    {
+        period: '施工收尾',
+        state: '执行中',
+        principal: '杨磊',
+        time: '2017/11/11-2017/12/12'
+    },
+    {
+        period: '施工收尾',
+        state: '执行中',
+        principal: '杨磊',
+        time: '2017/11/11-2017/12/12'
+    },
+];
+let tempArr = dataArr;
+
+export default class MyPlan extends Component {
+    constructor(props) {
+        super(props);
+        this.dataSource = dataArr;
+        this.state = {
+            hasMoreData: true,
+            list: (new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})).cloneWithRows(this.dataSource),
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <PullList
+                    onPullRelease={this.onPullRelease.bind(this)}
+                    topIndicatorRender={this.topIndicatorRender.bind(this)}
+                    topIndicatorHeight={60}
+                    dataSource={this.state.list}
+                    renderRow={this.renderRow.bind(this)}
+                    onEndReached={this.loadMore.bind(this)}
+                    onEndReachedThreshold={60}
+                    renderFooter={this.renderFooter.bind(this)}
+                />
+            </View>
+        )
+    }
+
+    onPullRelease(resolve) {
+        //do refresh
+        setTimeout(() => {
+            resolve();
+        }, 3000);
+    }
+
+    renderRow(item, sectionID, rowID, highlightRow) {
+        return (
+            <MyPlanCell key={rowID} data={item} navigator={this.props.navigator}
+                              setModalVisible={() => this.props.setModalVisible()}/>
+        );
+    }
+
+    renderFooter (){
+        return (this.state.hasMoreData ? <LoadMore /> : null)
+    }
+
+    topIndicatorRender(pulling, pullok, pullrelease) {
+        return (<Reload />);
+    }
+
+    loadMore(){
+        for (let i = 0;i<tempArr.length;i++){
+            this.dataSource.push(tempArr[i])
+        }
+
+        setTimeout(() => {
+            this.setState({
+                list: this.state.list.cloneWithRows(this.dataSource)
+            });
+        }, 1000);
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f2f2f2'
+    }
+});
---
+++ main/Home/Applications/ConstructionProgressPlan/Component/MyPlanCell.js
@@ -0,0 +1,99 @@
+/**
+ * Created by zhubin on 17/5/9.
+ */
+
+'use strict';
+import React, {Component} from 'react'
+import {
+    View,
+    StyleSheet,
+    Dimensions,
+    Text,
+    TouchableOpacity,
+    Image
+} from 'react-native'
+const {width} = Dimensions.get('window');
+
+
+export default class MyPlanCell extends Component {
+    render() {
+        return (
+            <TouchableOpacity style={styles.earlierStageListCell} onPress={this.skipPage.bind(this)}>
+                <View style={styles.aboutProject}>
+                    <Text style={{color:'#216fd0',fontSize:width*0.045}}>{this.props.data.period}</Text>
+                    <View style={styles.blank}/>
+                    <View style={styles.stateView}>
+                        <Text style={styles.stateText}>{this.props.data.state}</Text>
+                    </View>
+                </View>
+                <View style={styles.aboutPrincipal}>
+                    <Text style={[{width: width * 0.2}, styles.textStyle]}>{this.props.data.principal}</Text>
+                    <Text style={[{width: width * 0.5}, styles.textStyle]}>{this.props.data.time}</Text>
+                    <View style={styles.blank}/>
+                    <TouchableOpacity onPress={()=>{this.props.setModalVisible()}} style={styles.editTouch}>
+                        <Image source={require('../../../../../resource/imgs/home/earlierStage/edit.png')}
+                               style={styles.editImg} resizeMode="contain"/>
+                    </TouchableOpacity>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+    skipPage() {
+
+    }
+}
+
+const styles = StyleSheet.create({
+    earlierStageListCell: {
+        marginBottom: width * 0.03,
+        marginLeft: width * 0.02,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        width: width * 0.96,
+    },
+    aboutProject: {
+        backgroundColor: '#fff',
+        height: width * 0.12,
+        paddingLeft: width * 0.02,
+        paddingRight: width * 0.02,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    aboutPrincipal: {
+        backgroundColor: '#f6f9fa',
+        height: width * 0.12,
+        paddingLeft: width * 0.02,
+        paddingRight: width * 0.02,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center'
+    },
+    projectName: {
+        height: width * 0.1,
+        lineHeight: 21
+    },
+    textStyle: {
+        color:'#4f74a3'
+    },
+    stateView: {
+        backgroundColor: '#fe9a25',
+        width:width*0.17,
+        height:width*0.05,
+        borderRadius:3,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    stateText: {
+        color: '#fff',
+        fontSize: width * 0.03
+    },
+    blank: {
+        flex: 1
+    },
+    editImg: {
+        width: 22,
+        height: 25
+    }
+
+});
