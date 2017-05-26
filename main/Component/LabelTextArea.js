ad1af2289583bb40b10bc085196d7562ac024eab
zhubin <foreverscmd@gmail.com>
2017-05-26 23:20:07 +0800

    质量检查纪录-2

---
+++ main/Component/LabelTextArea.js
@@ -0,0 +1,58 @@
+/**
+ * Created by zhubin on 17/5/26.
+ */
+'use strict';
+import React, {Component} from 'react'
+import {
+    View,
+    StyleSheet,
+    Dimensions,
+    Text,
+    TextInput
+} from 'react-native'
+
+const {width} = Dimensions.get('window');
+
+export default class LabelTextArea extends Component {
+    render() {
+        return (
+            <View style={styles.textArea}>
+                <View style={styles.row}>
+                    <Text style={[styles.labelColor]}>
+                        {this.props.label}
+                    </Text>
+                </View>
+                <View style={styles.textContent}>
+                    <TextInput
+                        multiline = {true}
+                        numberOfLines = {4}
+                        placeholder="请填写"
+                        style={{height: 0.20 * width }}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    row: {
+        paddingLeft: width*0.02,
+        paddingRight: width*0.02,
+        justifyContent: 'center',
+        height: 0.12 * width,
+        borderBottomWidth: 1,
+        borderBottomColor: '#dcdcdc',
+
+    },
+    labelColor: {
+        color: '#5476a1'
+    },
+    textArea: {
+        marginTop: 0.02 * width,
+        backgroundColor: 'white'
+    },
+    textContent: {
+        padding: 0.02 * width
+    }
+});
--- main/Home/Applications/QualityCheckRecord/Component/QualityCheckRecordDetail.js
+++ main/Home/Applications/QualityCheckRecord/Component/QualityCheckRecordDetail.js
@@ -11,15 +11,24 @@ import {
     TouchableOpacity,
     Image,
     ScrollView,
-    TextInput
+    TextInput,
+    Switch
 } from 'react-native'
 import StatusBar from "../../../../Component/StatusBar"
 import KeyValueRight from "../../../../Component/KeyValueRight"
 import KeyTime from "../../../../Component/KeyTime"
+import LabelTextArea from "../../../../Component/LabelTextArea"
 
 const {width} = Dimensions.get('window');
 
 export default class QualityCheckRecordDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFinished: false
+        };
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -28,26 +37,40 @@ export default class QualityCheckRecordDetail extends Component {
                     <KeyValueRight propKey="检验任务" readOnly={true} defaultValue="质量检查计划1>"/>
                     <KeyValueRight propKey="工程工号" readOnly={true} defaultValue="CX_DS14034"/>
                     <KeyValueRight propKey="项目名称" readOnly={true} defaultValue="龙泽苑D区配电室改造工程"/>
-                    <KeyValueRight propKey="当前状态" readOnly={true} defaultValue="执行中"/>
                     <KeyValueRight propKey="工程子项名称" readOnly={true} defaultValue="项目实施计划任务"/>
                     <KeyValueRight propKey="工程节点" readOnly={true} defaultValue="施工进场"/>
                     <KeyTime propKey="检验时间"/>
                     <KeyValueRight propKey="检验人" readOnly={true} defaultValue="王娜>"/>
-                    <View style={styles.textArea}>
-                        <View style={styles.row}>
-                            <Text style={[styles.labelColor]}>
-                                检查结果
-                            </Text>
+                    <View style={styles.row}>
+                        <Text style={styles.labelColor}>附件</Text>
+                        <View style={styles.blank}/>
+                        <Image style={styles.icon} source={require('../../../../../resource/imgs/home/attachment.png')}/>
+                    </View>
+                    <View style={styles.attachment}>
+                        <View style={styles.attachmentLabel}>
+                            <Text style={{color: '#666'}}>文件名.pdf</Text>
                         </View>
-                        <View style={styles.textContent}>
-                            <TextInput
-                                multiline = {true}
-                                numberOfLines = {4}
-                                placeholder="请填写"
-                                style={{backgroundColor: 'white', height: 0.20 * width }}
-                            />
+                        <View style={styles.attachmentContent}>
+                            <View style={styles.square}>
+                                <Text style={{fontSize: 0.1 * width, color: "#d2d2d2"}}>+</Text>
+                            </View>
                         </View>
                     </View>
+                    <LabelTextArea label="检查结果"/>
+                    <LabelTextArea label="整改要求"/>
+                    <View style={styles.divide}/>
+                    <View style={styles.row}>
+                        <Text style={styles.labelColor}>
+                            是否已现场整改
+                        </Text>
+                        <View style={styles.blank}/>
+                        <Switch onValueChange={(value) => this.toggle(value)}
+                                value={this.state.isFinished}/>
+                    </View>
+                    <View style={styles.divide}/>
+                    <KeyTime propKey="创建时间"/>
+                    <KeyValueRight propKey="创建人" readOnly={true} defaultValue="王蒙"/>
+
                 </ScrollView>
                 <View style={styles.actionPanel}>
                     <TouchableOpacity onPress={() => this.submit()}>
@@ -67,6 +90,9 @@ export default class QualityCheckRecordDetail extends Component {
     }
     submit() {
 
+    }
+    toggle(value) {
+        this.setState({isFinished: value})
     }
 }
 
@@ -95,11 +121,15 @@ const styles = StyleSheet.create({
     row: {
         paddingLeft: width*0.02,
         paddingRight: width*0.02,
-        justifyContent: 'center',
         height: 0.12 * width,
         borderBottomWidth: 1,
         borderBottomColor: '#dcdcdc',
-
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        alignItems: 'center'
+    },
+    blank: {
+        flex: 1
     },
     buttonText: {
         color: 'white'
@@ -113,5 +143,36 @@ const styles = StyleSheet.create({
     },
     textContent: {
         padding: 0.02 * width
+    },
+    divide: {
+        height: 0.02 * width
+    },
+    icon: {
+        width:width * 0.05,
+        height:width * 0.05
+    },
+    attachment: {
+        paddingLeft: 0.02 * width,
+        paddingRight: 0.02 * width,
+        backgroundColor: 'white'
+    },
+    attachmentLabel: {
+        height: 0.12 * width,
+        justifyContent: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#dcdcdc'
+    },
+    attachmentContent: {
+        paddingTop: 0.02 * width,
+        paddingBottom: 0.02 * width
+    },
+    square: {
+        height: 0.2 * width,
+        width: 0.2 * width,
+        borderWidth: 1.5,
+        borderColor: '#d2d2d2',
+        borderStyle: 'dashed',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
