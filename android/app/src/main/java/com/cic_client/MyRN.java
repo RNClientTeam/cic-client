9c55ddab4ad3c7537f3f893ee2e93d06a9b7a495
xinker <yuexin@huanqiu.com>
2017-06-26 15:12:06 +0800

    pdf3

--- android/app/src/main/java/com/cic_client/MyRN.java
+++ android/app/src/main/java/com/cic_client/MyRN.java
@@ -42,10 +42,13 @@ public class MyRN extends ReactContextBaseJavaModule {
         try{
             Activity currentActivity=getCurrentActivity();
             if(currentActivity!=null) {
-                Intent intent = new Intent(currentActivity,pdf.class);
+                Toast.makeText(currentActivity,"添加附件",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
                 currentActivity.startActivityForResult(intent,1);
                 //进行回调数据
-                successBack.invoke(pdf.mQueue.take());
+                successBack.invoke(MainActivity.mQueue.take());
             }
         }catch (Exception e){
             errorBack.invoke(e.getMessage());
