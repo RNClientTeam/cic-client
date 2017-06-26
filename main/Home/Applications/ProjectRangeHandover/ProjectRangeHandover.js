45a29d1945ba28f0540eb562efa695dd7ebbf563
Nealyang <nealyang231@gmail.com>
2017-06-26 22:41:15 +0800

    工程子项拆分列表页

--- main/Home/Applications/ProjectRangeHandover/ProjectRangeHandover.js
+++ main/Home/Applications/ProjectRangeHandover/ProjectRangeHandover.js
@@ -17,6 +17,8 @@ import SearchHeader from '../Component/SearchHeader'
 import ProjectRangeHandoverList from './Component/ProjectRangeHandoverList'
 import ProjectRangeHandoverModal from "./Component/ProjectRangeHandoverModal";
 import {getCurrentMonS,getCurrentMonE} from '../../../Util/Util'
+import Loading from "../../../Component/Loading";
+import toast from 'react-native-simple-toast'
 export default class ProjectRangeHandover extends Component{
     constructor(props){
         super(props);
@@ -25,7 +27,9 @@ export default class ProjectRangeHandover extends Component{
             sDate:getCurrentMonS(),
             eDate:getCurrentMonE(),
             xmmc:'',
-            pageNum:1
+            pageNum:1,
+            list:[],
+            isLoading:false
         }
     }
 
@@ -37,18 +41,59 @@ export default class ProjectRangeHandover extends Component{
                         <Image style={styles.filtrate} source={require('../../../../resource/imgs/home/earlierStage/filtrate.png')}/>
                     </TouchableOpacity>
                 </StatusBar>
-                <SearchHeader sDate={this.state.sDate} eDate={this.state.eDate}/>
-                <ProjectRangeHandoverList navigator={this.props.navigator}/>
-                {this.state.isModalVisible?<ProjectRangeHandoverModal sDate={this.state.sDate} eDate={this.state.eDate} isModalVisible={this.state.isModalVisible}  closeModal={()=>this.setState({isModalVisible:false})} />:<View></View>}
+                <SearchHeader
+                    getData={()=>{this.getDateFromNet(1)}}
+                    getKeyWord={(txt)=>{this.setState({xmmc:txt})}}/>
+                <ProjectRangeHandoverList
+                    getData={(pageNum,resolve)=>this.getDateFromNet(pageNum,resolve)}
+                    loadMore={()=>this.loadMore()}
+                    dataSource={this.state.list} navigator={this.props.navigator}/>
+                {this.state.isModalVisible?<ProjectRangeHandoverModal
+                    sDate={this.state.sDate}
+                    eDate={this.state.eDate}
+                    isModalVisible={this.state.isModalVisible}
+                    closeModal={()=>this.setState({isModalVisible:false})}
+                    changeFileter={(sDate,eDate)=>this.changeFileter(sDate,eDate)}
+                />:<View></View>}
+                {this.state.isLoading?<Loading/>:null}
             </View>
         )
+    }
+
+    showLoading(){
+        this.setState({
+            isLoading:true
+        })
+    }
+    hideLoading(){
+        this.setState({
+            isLoading:false
+        })
     }
 
     componentDidMount() {
         this.getDateFromNet(1,()=>{});
     }
 
-    getDateFromNet(pageNum,resolve){
+    changeFileter(sdate,edate){
+        this.setState({
+            sDate:sdate,
+            eDate:edate
+        },function () {
+            this.getDateFromNet(1);
+        })
+    }
+
+    loadMore(){
+        this.setState({
+            pageNum:this.state.pageNum+1
+        },function () {
+            this.getDateFromNet(this.state.pageNum)
+        })
+    }
+
+    getDateFromNet(pageNum,resolve=()=>{}){
+        this.showLoading();
         axios.get('/psmGcfw/xmlist',{
             params:{
                 userID:GLOBAL_USERID,
@@ -60,39 +105,70 @@ export default class ProjectRangeHandover extends Component{
                 xmmc:this.state.xmmc
             }
         }).then(data=>{
-            console.log(data);
+            this.hideLoading();
             // TODO
-            data = {
-                "code": 1,
-                "data": {
-                    "total": 13,
-                    "list": [
-                        {
-                            "id": "8a8180b85c0b0744015c0b0e6d45004b",
-                            "gcfwjjztmc": "已拆分子项",
-                            "zxcount": "0",
-                            "xmjl": "刘姗姗",
-                            "gcfwjjzt": 60,
-                            "xmmc": "京棉A1区北区2号配及A2区2#配户表集中器",
-                            "xmbh": "DQ11104-14161",
-                            "ssdw": "市场营销二部",
-                            "cfsj": "2017-05-15 00:00:00"
-                        },
-                        {
-                            "id": "8a8180b85bacee8d015bad56dfc10306",
-                            "gcfwjjztmc": "已拆分子项",
-                            "zxcount": "1",
-                            "xmjl": "贾世坤",
-                            "gcfwjjzt": 60,
-                            "xmmc": "2016平谷南营路网架结构优化",
-                            "xmbh": "CX_DS16003-16016",
-                            "ssdw": "市场营销一部",
-                            "cfsj": "2017-04-27 00:00:00"
-                        }
-                    ]
-                },
-                "message": "成功"
+            if(pageNum<10){
+                data = {
+                    "code": 1,
+                    "data": {
+                        "total": 13,
+                        "list": [
+                            {
+                                "id": "8a8180b85c0b0744015c0b0e6d45004b",
+                                "gcfwjjztmc": "已拆分子项",
+                                "zxcount": "0",
+                                "xmjl": "刘姗姗",
+                                "gcfwjjzt": 60,
+                                "xmmc": "京棉A1区北区2号配及A2区2#配户表集中器",
+                                "xmbh": "DQ11104-14161",
+                                "ssdw": "市场营销二部",
+                                "cfsj": "2017-05-15 00:00:00"
+                            },
+                            {
+                                "id": "8a8180b85bacee8d015bad56dfc10306",
+                                "gcfwjjztmc": "已拆分子项",
+                                "zxcount": "1",
+                                "xmjl": "贾世坤",
+                                "gcfwjjzt": 60,
+                                "xmmc": "2016平谷南营路网架结构优化",
+                                "xmbh": "CX_DS16003-16016",
+                                "ssdw": "市场营销一部",
+                                "cfsj": "2017-04-27 00:00:00"
+                            }
+                        ]
+                    },
+                    "message": "成功"
+                };
+            }else{
+                data = {
+                    "code": 1,
+                    "data": {
+                        "total": 13,
+                        "list": []
+                    },
+                    "message": "成功"
+                };
             }
+
+            if(data.code ===1){
+                resolve();
+                if(pageNum === 1){
+                    this.state.list = [];
+                }
+                for(let i = 0;i<data.data.list.length;i++){
+                    this.state.list.push(data.data.list[i]);
+                }
+                this.setState({
+                    list:this.state.list
+                });
+                if(data.data.list.length===0){
+                    return false
+                }else{
+                    return true
+                }
+            }
+        }).catch(err=>{
+            toast.show('服务端异常');
         })
     }
 }
